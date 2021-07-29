2cbfcec895273851de3821c539a9add8be90833f
DJ Taylor <dtaylor88@gmail.com>
2021-07-28 18:36:38 -0700

    commit tree 3074220c3ad1a339d5e585c8c3d856158c70627f

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+song_list = [
+    "Jolene",
+    "Another One Bites The Dust",
+    "Final Countdown",
+    "Forgot About Dre"
+  ]
+  
+  song_list.each do |name|
+    Song.create(name: name)
+  end
+
+  artist_list = [
+    [ "Dolly", "I'm a famous country musician!"],
+    [ "Queen", "Freddy Mercury, duh"],
+    [ "AC/DC", "Who wrote this song?"],
+    [ "Dr. Dre", "Featuring Eminem"]
+  ]
+  
+  artist_list.each do |name, text|
+    Artist.create(name: name, bio: text)
+  end
+
+  genre_list = [
+    "Country",
+    "Rock",
+    "Hip Hop",
+    "Funk"
+  ]
+  
+  genre_list.each do |name|
+    Genre.create(name: name)
+  end
+  
